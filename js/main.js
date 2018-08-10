819aca3da5e4f389a92acb64265561bf80f1dc2f
Neon Knight <lgaston@myhoosierneighbors.com>
2018-08-10 11:17:00 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ document.getElementById("tanktrouble").onclick = function () {
         location.href = "tanktrouble.html";
 };
 document.getElementById("legendofzelda").onclick = function () {
-        location.href = "https://neonk.github.io/zelda/www/index.html";
+        window.location.replace("https://neonk.github.io/zelda/www/index.html");
 };
 document.getElementById("zombo").onclick = function () {
         location.href = "zombocalypse.html";
