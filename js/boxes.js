e7f2fdb2450f3c8af8bdf8e68a41eab4b854b035
Neon Knight <lgaston@myhoosierneighbors.com>
2018-08-14 14:15:16 -0400

    Update boxes.js

--- js/boxes.js
+++ js/boxes.js
@@ -1,6 +1,6 @@
 function loadBoxes() {
   document.getElementById("box1text").innerHTML = "BTD5"
   document.getElementById("box1img").src = "images/btd5.jpg"
-  document.getElementByClassName("box1button").id = "btd5"
+  document.getElementByClassName("box1button").onclick = "btd5()"
 }
 window.onload = loadBoxes;
