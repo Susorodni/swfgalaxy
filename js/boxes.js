5c30816637b9d96ee5b88795576e2ba4404961b9
Neon Knight <lgaston@myhoosierneighbors.com>
2018-08-15 09:57:40 -0400

    Fixed image name

--- js/boxes.js
+++ js/boxes.js
@@ -5,7 +5,7 @@ function loadBoxes()
   document.getElementById("box1button").onclick = btd5;
   // Box 2
   document.getElementById("box2text").innerHTML = "Duck Life 4"
-  document.getElementById("box2img").src = "images/ducklife4.jpeg"
+  document.getElementById("box2img").src = "images/ducklife-4.jpeg"
   document.getElementById("box2button").onclick = ducklife4;
 }
 window.onload = loadBoxes;
