0cfdc738ff05a3a45ccc5e237a348facdad53d9d
Neon Knight <lgaston@myhoosierneighbors.com>
2018-08-15 15:14:21 -0400

    Delete boxes.js

--- js/boxes.js
+++
@@ -1,6 +0,0 @@
-function loadBoxes() 
-  document.getElementById("box1text").innerHTML = "BTD5"
-  document.getElementById("box1img").src = "images/btd5.jpg"
-  document.getElementById("box1button").onclick = btd5;
-}
-loadBoxes()
