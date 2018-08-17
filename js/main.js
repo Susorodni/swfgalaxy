656898adf3a31b7c528ce03046dc8dcf1e8617cd
Neon Knight <lgaston@myhoosierneighbors.com>
2018-08-17 14:57:41 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 document.getElementById("btd5").onclick = function {
         location.href = "btd5.html";
-    };
+};
 document.getElementById("ducklife4").onclick = function {
         location.href = "ducklife4.html";
-    };
+};
 document.getElementById("flappybird").onclick = function () {
         location.href = "flappybird.html";
-    };
+};
 document.getElementById("geodash").onclick = function () {
         location.href = "geometrydash.html";
 };
 document.getElementById("giveup2").onclick = function () {
         location.href = "giveup2.html";
-    };
+};
 document.getElementById("happywheels").onclick = function () {
         location.href = "happywheels.html";
-    };
+};
 document.getElementById("jacksmith").onclick = function () {
         location.href = "jacksmith.html";
-    };
+};
 document.getElementById("learntofly3").onclick = function () {
         location.href = "learntofly3.html";
-    };
+};
 document.getElementById("papasbakeria").onclick = function () {
         location.href = "papasbakeria.html";
-    };
+};
 document.getElementById("papasdonuteria").onclick = function () {
         location.href = "papasdonuteria.html";
-    };
+};
 document.getElementById("pokemontowerdefense").onclick = function () {
         location.href = "pokemontowerdefense.html";
 };
