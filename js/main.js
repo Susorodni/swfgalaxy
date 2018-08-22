7c3b02cb78a074a4deea45c7c502c9a647297f1d
Neon Knight <lgaston@myhoosierneighbors.com>
2018-08-21 21:46:13 -0400

    The New Update!
    Added a system to add games easier and added three new games.

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ function loadBoxes () {
     document.getElementById("t6").innerHTML = "Happy Wheels";
     document.getElementById("i6").src = "images/happywheels.jpeg";
     document.getElementById("b6").onclick = function () { happywheels(); };
+    // Impossible Quiz
+    document.getElementById("n1").onclick = function () { impossiblequiz(); };
     // Jacksmith
     document.getElementById("t7").innerHTML = "Jacksmith";
     document.getElementById("i7").src = "images/jacksmith.jpeg";
@@ -31,60 +33,116 @@ function loadBoxes () {
     document.getElementById("t8").innerHTML = "Learn to Fly 3";
     document.getElementById("i8").src = "images/learntofly3.jpeg";
     document.getElementById("b8").onclick = function () { learntofly3(); };
-    // Box 9
+    // Papa's Bakeria
+    document.getElementById("t9").innerHTML = "Papa's Bakeria";
+    document.getElementById("i9").src = "images/papasbakeria.jpeg";
+    document.getElementById("b9").onclick = function () { papasbakeria(); };
+    // Papa's Donuteria
+    document.getElementById("t10").innerHTML = "Papa's Donuteria";
+    document.getElementById("i10").src = "images/papasdonuteria.jpeg";
+    document.getElementById("b10").onclick = function () { papasdonuteria(); };
+    // Pokemon Tower Defense
+    document.getElementById("t11").innerHTML = "Pokemon Tower Defense";
+    document.getElementById("i11").src = "images/pokemontowerdefense.jpeg";
+    document.getElementById("b11").onclick = function () { pokemontowerdefense(); };
+    // Run 3
+    document.getElementById("n2").onclick = function () { run3(); };
+    // Strike Force Heroes
+    document.getElementById("t12").innerHTML = "Strike Force Heroes";
+    document.getElementById("i12").src = "images/strikeforceheroes.jpeg";
+    document.getElementById("b12").onclick = function () { strikeforceheroes(); };
+    // Strike Force Heroes 2
+    document.getElementById("t13").innerHTML = "Strike Force Heroes 2";
+    document.getElementById("i13").src = "images/strikeforceheroes2.jpeg";
+    document.getElementById("b13").onclick = function () { strikeforceheroes2(); };
+    // Sports Heads Basketball Championship
+    document.getElementById("t14").innerHTML = "Sports Heads Basketball Championship";
+    document.getElementById("i14").src = "images/shbc.jpeg";
+    document.getElementById("b14").onclick = function () { sportsheadsbasketball(); };
+    // Super Smash Flash
+    document.getElementById("t15").innerHTML = "Super Smash Flash";
+    document.getElementById("i15").src = "images/supersmashflash.jpeg";
+    document.getElementById("b15").onclick = function () { supersmashflash(); };
+    // Super Smash Flash 2
+    document.getElementById("n3").onclick = function () { supersmashflash2(); };
+    // Swords and Sandals 2
+    document.getElementById("t16").innerHTML = "Swords and Sandals 2";
+    document.getElementById("i16").src = "images/swordsandsandals2.jpg";
+    document.getElementById("b16").onclick = function () { swordsandsandals2(); };
+    // Tank Trouble
+    document.getElementById("t17").innerHTML = "Tank Trouble";
+    document.getElementById("i17").src = "images/tanktrouble.jpeg";
+    document.getElementById("b17").onclick = function () { tanktrouble(); };
+    // Legend of Zelda
+    document.getElementById("t18").innerHTML = "Legend of Zelda";
+    document.getElementById("i18").src = "images/legendofzelda.jpeg";
+    document.getElementById("b18").action = "https://neonk.github.io/zelda/www/index.html";
+    // Zombocalypse
+    document.getElementById("t19").innerHTML = "Zombocalypse";
+    document.getElementById("i19").src = "images/zombo.jpeg";
+    document.getElementById("b19").onclick = function () { zombocalypse(); };
 }
 window.onload = loadBoxes
-function btd5 () {
+function btd5() {
         location.href = "btd5.html";
 };
-function ducklife4 () {
+function ducklife4() {
         location.href = "ducklife4.html";
 };
-function flappybird () {
+function flappybird() {
         location.href = "flappybird.html";
 };
-function geodash () {
+function geodash() {
         location.href = "geometrydash.html";
 };
-function giveup2 () {
+function giveup2() {
         location.href = "giveup2.html";
 };
-function happywheels () {
+function happywheels() {
         location.href = "happywheels.html";
 };
-function jacksmith () {
+function jacksmith() {
         location.href = "jacksmith.html";
 };
-function learntofly3 () {
+function learntofly3() {
         location.href = "learntofly3.html";
 };
-document.getElementById("papasbakeria").onclick = function () {
+function papasbakeria() {
         location.href = "papasbakeria.html";
 };
-document.getElementById("papasdonuteria").onclick = function () {
+function papasdonuteria() {
         location.href = "papasdonuteria.html";
 };
-document.getElementById("pokemontowerdefense").onclick = function () {
+function pokemontowerdefense() {
         location.href = "pokemontowerdefense.html";
 };
-document.getElementById("strikeforceheroes").onclick = function () {
+function strikeforceheroes() {
         location.href = "strikeforceheroes.html";
 };
-document.getElementById("strikeforceheroes2").onclick = function () {
+function strikeforceheroes2() {
         location.href = "strikeforceheroes2.html";
 };
-document.getElementById("sportsheads").onclick = function () {
+function sportsheadsbasketball() {
         location.href = "sportsheadsbasketballchampionship.html";
 };
-document.getElementById("supersmashflash").onclick = function () {
+function supersmashflash() {
         location.href = "supersmashflash.html";
 };
-document.getElementById("swordsandsandals2").onclick = function () {
+function swordsandsandals2() {
         location.href = "swordsandsandals2.html";
 };
-document.getElementById("tanktrouble").onclick = function () {
+function tanktrouble() {
         location.href = "tanktrouble.html";
 };
-document.getElementById("zombo").onclick = function () {
+function zombocalypse() {
         location.href = "zombocalypse.html";
 };
+function impossiblequiz() {
+        location.href = "impossiblequiz.html"
+};
+function run3() {
+        location.href = "run3.html"
+}
+function supersmashflash2() {
+        location.href = "supersmashflash2.html"
+}
